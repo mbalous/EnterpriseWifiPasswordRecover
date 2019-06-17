8c1004bd4f4c63074b102f43b0dc99035472089e
mbalous <mbalous@outlook.com>
2019-06-17 12:03:16 +0200

    Refactoring

--- EnterpriseWifiPasswordRecover/Program.cs
+++ EnterpriseWifiPasswordRecover/Program.cs
@@ -1,6 +1,8 @@
 ﻿using Microsoft.Win32;
 using System;
 using System.Collections.Generic;
+using System.IO;
+using System.Linq;
 using System.Security.Cryptography;
 using System.Text;
 
@@ -9,18 +11,22 @@ namespace EnterpriseWifiPasswordRecover
     class Program
     {
         // The files we will store the output into
-        public static string workingDirectory = System.IO.Directory.GetCurrentDirectory() + "/profiles/";
-        public static string stage1Prefix = "stage1_";
-        public static string stage2Prefix = "stage2_";
-        public static string stage3Prefix = "decrypted_";
-        public static string extension = ".txt";
+        public string _workingDirectory = Directory.GetCurrentDirectory() + @"\profiles\";
+        public const string stage1Prefix = "stage1_";
+        public const string stage2Prefix = "stage2_";
+        public const string stage3Prefix = "decrypted_";
+        public const string extension = ".txt";
 
-        static void Main(string[] args)
+        static void Main(string[] args) => new Program();
+
+        public Program()
         {
+            Console.WriteLine($"Application started as user '{Environment.UserName}'");
+
             // Ensure we have a profiles directory
             try
             {
-                System.IO.Directory.CreateDirectory(workingDirectory);
+                Directory.CreateDirectory(this._workingDirectory);
             }
             catch
             {
@@ -33,8 +39,11 @@ namespace EnterpriseWifiPasswordRecover
             Stage3();   // Show creds, where possible   Typically runas: the user that owns the connection
         }
 
-        // Retreive the payloads from registry
-        public static void Stage1()
+        /// <summary>
+        /// Retreive the payloads from registry.
+        /// Run as "nt authority\system" to retreive keys of every user.
+        /// </summary>
+        public void Stage1()
         {
             // The location where the magic is stored in the registry
             string keyName = @"Software\Microsoft\Wlansvc\UserData\Profiles\";
@@ -43,12 +52,12 @@ namespace EnterpriseWifiPasswordRecover
             // Will contain a list of places to search in the registry for profiles
             List<RegistryKey> possibleKeys = new List<RegistryKey>();
 
-            // Try access all other user's registries
             try
             {
+                // Try access all other user's registries
                 string[] allUsers = Registry.Users.GetSubKeyNames();
 
-                for(int i=0; i<allUsers.Length; ++i)
+                for (int i = 0; i < allUsers.Length; ++i)
                 {
                     try
                     {
@@ -69,7 +78,7 @@ namespace EnterpriseWifiPasswordRecover
             {
                 // Error access users registry store
             }
-            
+
             // Try to open the local machine entry
             try
             {
@@ -92,13 +101,11 @@ namespace EnterpriseWifiPasswordRecover
                 // Do nothing
             }
 
+
             // Try both registry locations
-            for (int i = 0; i < possibleKeys.Count; ++i)
+            foreach (RegistryKey key in possibleKeys.Where(key => key != null))
             {
                 // Grab the registry location
-                RegistryKey key = possibleKeys[i];
-                
-                if (key == null) continue;
                 try
                 {
                     // Grab every subkey in here, each profile has a subkey
@@ -117,7 +124,7 @@ namespace EnterpriseWifiPasswordRecover
                         if (theData != null)
                         {
                             // Write it to disk, this is stage1 extracted!
-                            System.IO.File.WriteAllBytes(workingDirectory + "/" + stage1Prefix + subKey + extension, theData);
+                            File.WriteAllBytes(this._workingDirectory + "/" + stage1Prefix + subKey + extension, theData);
                             Console.WriteLine("Extracted stage1 for " + subKey);
                         }
                     }
@@ -130,15 +137,18 @@ namespace EnterpriseWifiPasswordRecover
             }
         }
 
-        // Decrypt the overall package, this gets us the domain + username + encrypted password
-        public static void Stage2()
+        /// <summary>
+        /// Decrypt the overall package, this gets us the domain + username + encrypted password.
+        /// Run as "nt authority\system".
+        /// </summary>
+        public void Stage2()
         {
             string[] profiles;
 
             // Find a list of profiles in the profile directory we created in stage1
             try
             {
-                profiles = System.IO.Directory.GetFiles(workingDirectory);
+                profiles = Directory.GetFiles(this._workingDirectory);
             }
             catch
             {
@@ -148,48 +158,47 @@ namespace EnterpriseWifiPasswordRecover
             }
 
             // Loop over all the files
-            for(int i=0; i<profiles.Length; ++i)
+            foreach (string profilePath in profiles)
             {
-                string profilePath = profiles[i];
-                string profile = System.IO.Path.GetFileNameWithoutExtension(profilePath);
+                string profile = Path.GetFileNameWithoutExtension(profilePath);
 
                 // Is this a stage1 profile?
-                if (!profile.Contains(stage1Prefix)) continue;
+                if (!profile.Contains(stage1Prefix))
+                    continue;
                 profile = profile.Replace(stage1Prefix, "");
 
                 try
                 {
                     // Read in the data from this stage
-                    byte[] data = System.IO.File.ReadAllBytes(profilePath);
+                    byte[] data = File.ReadAllBytes(profilePath);
 
                     // Try to decrypt it, and then store it into a stage2 file
                     try
                     {
                         byte[] unprotectedBytes = ProtectedData.Unprotect(data, null, DataProtectionScope.LocalMachine);
-                        System.IO.File.WriteAllBytes(workingDirectory + "/" + stage2Prefix + profile + extension, unprotectedBytes);
+                        File.WriteAllBytes(this._workingDirectory + "/" + stage2Prefix + profile + extension, unprotectedBytes);
                     }
                     catch
                     {
-                        Console.WriteLine("Failed to decrypt " + profile + " - Run as SYSTEM or ORIGINAL USER!");
+                        Console.WriteLine($"Failed to decrypt {profile} - Run as SYSTEM or ORIGINAL USER!");
                     }
                 }
                 catch
                 {
                     // Failed to read, continued
                     Console.WriteLine("Failed to read " + profile);
-                    continue;
                 }
             }
         }
 
         // Try to decrpyt the password, and extract username + domain
-        public static void Stage3()
+        public void Stage3()
         {
             string[] profiles;
 
             try
             {
-                profiles = System.IO.Directory.GetFiles(workingDirectory);
+                profiles = Directory.GetFiles(this._workingDirectory);
             }
             catch
             {
@@ -199,32 +208,33 @@ namespace EnterpriseWifiPasswordRecover
             }
 
             // Loop over all the profiles in the profile directory we created in the previous steps
-            for (int i = 0; i < profiles.Length; ++i)
+            foreach (string profilePath in profiles)
             {
-                string profilePath = profiles[i];
-                string profile = System.IO.Path.GetFileNameWithoutExtension(profilePath);
+                string profile = Path.GetFileNameWithoutExtension(profilePath);
 
                 // Is this a stage2 decrypted payload?
-                if (!profile.Contains(stage2Prefix)) continue;
+                if (!profile.Contains(stage2Prefix))
+                    continue;
                 profile = profile.Replace(stage2Prefix, "");
 
                 try
                 {
                     // Try to decrypt everything, and print what we found
-                    byte[] data = System.IO.File.ReadAllBytes(profilePath);
+                    byte[] data = File.ReadAllBytes(profilePath);
                     UsernameStore theStore = DecryptAll(data);
                     PrintUsernameStore(theStore);
 
                     // If we decrypted the password, store the result into a file
-                    if(theStore.password != null && theStore.password.Length > 0)
+                    if (theStore.password != null && theStore.password.Length > 0)
                     {
                         // Generate the payload
-                        string theOutput = profile + "\n";
-                        theOutput += "Domain: " + theStore.domain + "\n";
-                        theOutput += "Username: " + theStore.username + "\n";
-                        theOutput += "Password: " + theStore.password + "\n";
-                        
-                        System.IO.File.WriteAllText(workingDirectory + "/" + stage3Prefix + profile + extension, theOutput);
+                        var sb = new StringBuilder();
+                        sb.AppendLine(profile);
+                        sb.AppendLine("Domain: " + theStore.domain);
+                        sb.AppendLine("Username: " + theStore.username);
+                        sb.AppendLine("Password: " + theStore.password);
+
+                        File.WriteAllText(this._workingDirectory + "/" + stage3Prefix + profile + extension, sb.ToString());
                     }
                 }
                 catch
@@ -245,7 +255,7 @@ namespace EnterpriseWifiPasswordRecover
                 bool broken = false;
                 for (int j = 0; j < needle.Length; ++j)
                 {
-                    if(invert)
+                    if (invert)
                     {
                         // If there is a match, it's not the right signature
                         if (needle[j] == hayStack[i + j])
@@ -253,7 +263,8 @@ namespace EnterpriseWifiPasswordRecover
                             broken = true;
                             break;
                         }
-                    } else
+                    }
+                    else
                     {
                         // If the bytes don't match, it's not the right signature
                         if (needle[j] != hayStack[i + j])
@@ -279,16 +290,21 @@ namespace EnterpriseWifiPasswordRecover
         public static byte[] Slice(byte[] data, int startIndex = -1, int endIndex = -1)
         {
             // Sanity checking / argument fixing
-            if (startIndex < 0) startIndex = 0;
-            if (endIndex < 0 || endIndex > data.Length) endIndex = data.Length;
-            if (endIndex < startIndex) return new byte[0];
+            if (startIndex < 0)
+                startIndex = 0;
+
+            if (endIndex < 0 || endIndex > data.Length)
+                endIndex = data.Length;
+
+            if (endIndex < startIndex)
+                return new byte[0];
 
             // Allocate the memory for the new byte[] we will return
             int totalSize = endIndex - startIndex;
             byte[] toReturn = new byte[totalSize];
 
             // Copy the data into the new byte[]
-            for(int i=0; i<totalSize; ++i)
+            for (int i = 0; i < totalSize; ++i)
             {
                 toReturn[i] = data[i + startIndex];
             }
@@ -309,7 +325,8 @@ namespace EnterpriseWifiPasswordRecover
         public static UsernameStore DecryptAll(byte[] toDecrypt, UsernameStore theStore = null)
         {
             // Ensure we have a store
-            if (theStore == null) theStore = new UsernameStore();
+            if (theStore == null)
+                theStore = new UsernameStore();
 
             // Try to decrypt everything
             DecryptUsername(toDecrypt, theStore);
@@ -362,12 +379,12 @@ namespace EnterpriseWifiPasswordRecover
             else
             {
                 // Failed to find the blob, maybe it's not encrypted?
-                
+
                 usernameFieldStart = SigScan(toDecrypt, searchForUsername2);
 
                 Console.WriteLine(usernameFieldStart);
 
-                if(usernameFieldStart != -1)
+                if (usernameFieldStart != -1)
                 {
                     // Looks good?
 
@@ -384,11 +401,11 @@ namespace EnterpriseWifiPasswordRecover
                     // Keep searching, the password is probably here too!
                     int passowrdFieldStart = SigScan(toDecrypt, nullArray, usernameFieldEnd + 1, true);
 
-                    if(passowrdFieldStart != -1)
+                    if (passowrdFieldStart != -1)
                     {
                         int passwordFieldEnd = SigScan(toDecrypt, nullArray, passowrdFieldStart + 1);
 
-                        if(passwordFieldEnd != -1)
+                        if (passwordFieldEnd != -1)
                         {
                             byte[] passwordField = Slice(toDecrypt, passowrdFieldStart, passwordFieldEnd);
                             theStore.password = Encoding.ASCII.GetString(passwordField);
@@ -400,11 +417,11 @@ namespace EnterpriseWifiPasswordRecover
                             {
                                 int domainFieldEnd = SigScan(toDecrypt, nullArray, domainFieldStart + 1);
 
-                                if(domainFieldEnd != -1)
+                                if (domainFieldEnd != -1)
                                 {
                                     byte[] domainField = Slice(toDecrypt, domainFieldStart, domainFieldEnd);
 
-                                    if(domainField[0] != 0x01)
+                                    if (domainField[0] != 0x01)
                                     {
                                         theStore.domain = Encoding.ASCII.GetString(domainField);
                                     }
@@ -425,7 +442,7 @@ namespace EnterpriseWifiPasswordRecover
         // Attempt to decrypt the password field in the given payload, will store the password into the given UsernameStore, or create a new one
         public static UsernameStore DecryptPassword(byte[] toDecrypt, UsernameStore theStore = null)
         {
-            if(theStore == null) theStore = new UsernameStore();
+            if (theStore == null) theStore = new UsernameStore();
 
             // This is the signature of where the encrypted password starts
             byte[] searchForPassword = { 0x01, 0x00, 0x00, 0x00, 0xD0, 0x8C, 0x9D, 0xDF, 0x01 };
@@ -437,16 +454,15 @@ namespace EnterpriseWifiPasswordRecover
                 // A chunk was successfully found
                 Console.WriteLine("Found encrypted password blob...");
                 byte[] passwordBlob = Slice(toDecrypt, passwordFieldStart);
-
                 try
                 {
                     // Try to decrypt the chunk
                     byte[] unprotectedPassword = ProtectedData.Unprotect(passwordBlob, null, DataProtectionScope.LocalMachine);
 
                     // Strip any null bytes that were added
-                    for(int i=0; i<unprotectedPassword.Length; ++i)
+                    for (int i = 0; i < unprotectedPassword.Length; ++i)
                     {
-                        if(unprotectedPassword[i] == 0x00)
+                        if (unprotectedPassword[i] == 0x00)
                         {
                             unprotectedPassword = Slice(unprotectedPassword, 0, i);
                             break;
@@ -456,7 +472,7 @@ namespace EnterpriseWifiPasswordRecover
                     // Store the password
                     theStore.password = Encoding.ASCII.GetString(unprotectedPassword);
                 }
-                catch (Exception e)
+                catch
                 {
                     Console.WriteLine("Failed to decrypt password --- This needs to be run as the user who owns the password");
                     // failure
@@ -469,14 +485,17 @@ namespace EnterpriseWifiPasswordRecover
 
                 Console.WriteLine("Failed to find an encrypted password blob :/");
             }
-            
+
             return theStore;
         }
 
-        // This function decrypts a standard WPA2 PSK password
-        // Current unused
-        // Needs to be run as SYSTEM
-        public static string DecryptWirelessPassword(string encodedPassword)
+        /// <summary>
+        /// Decrypts a standard WPA2 PSK password
+        /// Needs to be run as SYSTEM
+        /// </summary>
+        /// <param name="encodedPassword"></param>
+        /// <returns></returns>
+        public static string DecryptWirelessPassword(string encodedPassword) // Currently unused
         {
             try
             {
